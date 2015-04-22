84e08dc97f32568c232a9c95bf9d2ea395f38465
Guillaume Rahbari <guillaume.rahbari@gmail.com>
2015-04-22 15:28:57 +0200

    init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name polySpottedApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the polySpottedApp
+ */
+angular.module('polySpottedApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
