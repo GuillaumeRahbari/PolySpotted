02175316a840bb85765ae3a940678e3ffcfdd58e
= <delmotte@polytech.unice.fr>
2015-05-06 14:56:32 +0200

    modif opti

--- front-end/app/scripts/app.js
+++ front-end/app/scripts/app.js
@@ -1,64 +1,49 @@
 'use strict';
 
 /**
- * @ngdoc overview
- * @name polySpottedApp
- * @description
- * # polySpottedApp
- *
- * Main module of the application.
- */
+* @ngdoc overview
+* @name polySpottedApp
+* @description
+* # polySpottedApp
+*
+* Main module of the application.
+*/
 angular
-  .module('polySpottedApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/dashboard.html',
-        controller: 'MasterCtrl'
-      })
-      .when('/tables', {
-        templateUrl: 'views/tables.html',
-        controller: 'AlertsCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-
-  })
-  .run(function ($rootScope, $cookieStore) {
-    /**
-     * Sidebar Toggle & Cookie Control
-     */
-    var mobileView = 992;
+.module('polySpottedApp', ['ngAnimate','ngCookies','ngResource','ngRoute','ngSanitize','ngTouch'])
+.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/dashboard.html',
+      controller: 'MasterCtrl'
+    })
+    .when('/tables', {
+      templateUrl: 'views/tables.html',
+      controller: 'AlertsCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+})
+.run(function ($rootScope, $cookieStore) {
 
     $rootScope.getWidth = function() {
-      return window.innerWidth;
+        return window.innerWidth;
     };
 
     $rootScope.$watch($rootScope.getWidth, function(newValue, oldValue) {
-      if (newValue >= mobileView) {
-
-        $rootScope.toggle = true;
-
-      } else {
-        $rootScope.toggle = false;
-      }
-
+        if (newValue >= 992) {
+            $rootScope.toggle = true;
+        } else {
+            $rootScope.toggle = false;
+        }
     });
 
     $rootScope.toggleSidebar = function() {
-      $rootScope.toggle = !$rootScope.toggle;
-      $cookieStore.put('toggle', $rootScope.toggle);
+        $rootScope.toggle = !$rootScope.toggle;
+        $cookieStore.put('toggle', $rootScope.toggle);
     };
 
     window.onresize = function() {
-      $rootScope.$apply();
+        $rootScope.$apply();
     };
-  });
+});
