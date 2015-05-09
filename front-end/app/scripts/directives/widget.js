9397504447d1de7a709b1386a1238d7598aefade
Guillaume Rahbari <guillaume.rahbari@gmail.com>
2015-05-09 15:50:59 +0200

    norme widget

--- front-end/app/scripts/directives/widget.js
+++ front-end/app/scripts/directives/widget.js
@@ -1,20 +1,17 @@
+'use strict';
+
 /**
  * Widget Directive
  */
 
 angular
     .module('polySpottedApp')
-    .directive('rdWidget', rdWidget);
+    .directive('rdWidget', function rdWidget() {
 
-function rdWidget() {
     var directive = {
         transclude: true,
         template: '<div class="widget" ng-transclude></div>',
         restrict: 'EA'
     };
     return directive;
-
-    function link(scope, element, attrs) {
-        /* */
-    }
-};
+});
