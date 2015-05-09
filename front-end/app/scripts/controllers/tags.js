8f73d756df1554a022d6d6653cdb6b8e7e9e7862
Guillaume Rahbari <guillaume.rahbari@gmail.com>
2015-05-09 15:42:16 +0200

    suppression des console.log

--- front-end/app/scripts/controllers/tags.js
+++ front-end/app/scripts/controllers/tags.js
@@ -13,7 +13,6 @@ angular.module('polySpottedApp')
   TagFactory.getTags().then(
     function (data) {
       $scope.tags = data;
-      console.log($scope.tags);
     }, function (msg) {
       console.log(msg);
     }
@@ -29,18 +28,13 @@ angular.module('polySpottedApp')
 
   $scope.numberTag = function(tag){
     var number = 0;
-    //console.log($scope.revelations);
     for (var revelation in $scope.revelations){
-      //console.log($scope.revelations[revelation]);
       for (var tag2 in $scope.revelations[revelation].tags){
-        //console.log($scope.revelations[revelation].tags[tag2]);
-
         if (tag === $scope.revelations[revelation].tags[tag2]){
           number++;
         }
       }
     }
-    //console.log(number);
     return number;
   };
 
