b523321875d4be47e50a4b4b10a55050c8d10045
Guillaume Rahbari <guillaume.rahbari@gmail.com>
2015-05-10 11:05:27 +0200

    trie des tags qui marche !

--- front-end/app/scripts/controllers/tags.js
+++ front-end/app/scripts/controllers/tags.js
@@ -8,35 +8,49 @@ angular.module('polySpottedApp')
     .controller('TagsCtrl', ['$scope', '$rootScope', 'TagFactory','RevelationFactory',
     function TagsCtrl($scope, $rootScope, TagFactory, RevelationFactory) {
 
-    //RevelationFactory.username = "delmotte";
-    $rootScope.title = 'Les Tags';
+      //RevelationFactory.username = "delmotte";
+      $rootScope.title = 'Les Tags';
 
-    TagFactory.getTags().then(
-      function (data) {
-        $scope.tags = data;
-      }, function (msg) {
-        console.log(msg);
-      }
-    );
+      RevelationFactory.getRevelations().then(
+        function (data) {
+          $scope.revelations = data;
+          TagFactory.getTags().then(
+            function (data) {
+              $scope.tags = [];
+              for (var tag in data){
+                var objet = {
+                  tagName : data[tag],
+                  nbOccurences : $scope.numberTag(data[tag])
+                };
+                $scope.tags.push(objet);
+              }
+              //console.log($scope.tags);
+              console.log($scope.tags);
+            }, function (msg) {
+              console.log(msg);
+            }
+          );
+          //console.log($scope.revelations);
+        }, function (msg) {
+          console.log(msg);
+        }
+      );
 
-    RevelationFactory.getRevelations().then(
-      function (data) {
-        $scope.revelations = data;
-      }, function (msg) {
-        console.log(msg);
-      }
-    );
-
-    $scope.numberTag = function(tag){
-      var number = 0;
-      for (var revelation in $scope.revelations){
-        for (var tag2 in $scope.revelations[revelation].tags){
-          if (tag === $scope.revelations[revelation].tags[tag2]){
-            number++;
+      $scope.numberTag = function(tag){
+        var number = 0;
+        console.log($scope.revelations);
+        for (var revelation in $scope.revelations){
+          console.log("cccc");
+          for (var tag2 in $scope.revelations[revelation].tags){
+            if (tag === $scope.revelations[revelation].tags[tag2]){
+              number++;
+              console.log("bbbbbbb");
+            }
           }
         }
-      }
-      return number;
-    };
+        return number;
+      };
+
+
 
 }]);
